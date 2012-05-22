510f394812b7362aabce35f7fcfb217d398e485d
Greg Unrein <greg@cedexis.com>
2012-05-22 16:01:45 -0700

    Updating update-api.sh to work across all new apps automatically, plus bringing the API docs up to date for avoid-by-geo

--- apps/avoid-by-geo/api/Utilities.php
+++ apps/avoid-by-geo/api/Utilities.php
@@ -6,7 +6,11 @@
 interface Utilities
 {
     /**
-     * Choose a random provider from the set of all options
+     * Choose a random provider from the set of all options.
+     *
+     * Example::
+     *
+     *      $utilities->selectRandom();
      */
     public function selectRandom();
 }
--- scripts/update-api.sh
+++ scripts/update-api.sh
@@ -8,4 +8,4 @@ while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # Update all of the apps using `template/api` as the source of truth
-ls $DIR/../apps/ | xargs -t -I \{\} rsync -a --delete $DIR/../template/api $DIR/../apps/\{\}
+ls $DIR/../apps/ | xargs -I \{\} rsync -a --delete $DIR/../apps/template/api $DIR/../apps/\{\}
