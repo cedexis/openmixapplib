4e313263dac5351ac9d2050f634d503c93938ca3
Jacob Wan <jacob@sequoia>
2012-11-28 17:10:26 -0800

    Add simple geo app

---
+++ apps/simple-geo/app/OpenmixApplication.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * For information on writing Openmix applications, check out
+ * https://github.com/cedexis/openmixapplib/wiki
+ */
+class OpenmixApplication implements Lifecycle
+{
+    private $providers = array(
+        'probe1.atl.tmd.prod' => '65.254.36.226',
+        'probe1.rbx.ovh.prod' => '94.23.254.99',
+        'probe1.sbg.ovh.prod' => '37.59.8.25',
+        'probe1.sin.sl.prod' => '119.81.23.243',
+        'probe1.sjc.sl.prod' => '50.97.227.68',
+    );
+    
+    private $ttl = 20;
+    
+    //if geo.market = NA or SA
+    //    round robin between (probe1.sjc.sl.prod, 50.97.227.68)
+    //                    and (probe1.atl.tmd.prod,65.254.36.226)
+    //  
+    //if geo.market= EU or AF
+    //    round-robin between (probe1.rbx.ovh.prod,94.23.254.99
+    //                    and (probe1.sbg.ovh.prod,37.59.8.25)
+    //  
+    //if geo.market = AS or OC
+    //    round-robin between (probe1.sin.sl.prod,119.81.23.243)
+    //                    and (probe1.sjc.sl.prod, 50.97.227.68)
+    //  
+    //if unknown
+    //    round robin between (probe1.atl.tmd.prod,65.254.36.226)
+    //                    and (probe1.sbg.ovh.prod,37.59.8.25)
+    
+    private $market_map = array(
+        'NA' => array( 'probe1.sjc.sl.prod', 'probe1.atl.tmd.prod' ),
+        'SA' => array( 'probe1.sjc.sl.prod', 'probe1.atl.tmd.prod' ),
+        'EU' => array( 'probe1.rbx.ovh.prod', 'probe1.sbg.ovh.prod' ),
+        'AF' => array( 'probe1.rbx.ovh.prod', 'probe1.sbg.ovh.prod' ),
+        'AS' => array( 'probe1.sin.sl.prod', 'probe1.sjc.sl.prod' ),
+        'OC' => array( 'probe1.sin.sl.prod', 'probe1.sjc.sl.prod' ),
+    );
+    
+    private $default_providers = array( 'probe1.atl.tmd.prod', 'probe1.sbg.ovh.prod' );
+    
+    /**
+     * @param Configuration $config
+     **/
+    public function init($config)
+    {
+        $config->declareInput(GeoProperties::MARKET);
+        
+        foreach ($this->providers as $alias => $cname) {
+            $config->declareResponseOption($alias, $cname, $this->ttl);
+        }
+    }
+    
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param Utilities $utilities
+     **/
+    public function service($request, $response, $utilities)
+    {
+        try {
+            $market = $request->geo(GeoProperties::MARKET);
+            //print("\nMarket: $market");
+            
+            $candidates = $this->default_providers;
+            if (array_key_exists($market, $this->market_map)) {
+                $candidates = $this->market_map[$market];
+            }
+            
+            $random = $this->rand(0, count($candidates) - 1);
+            //print("\nRandom: $random");
+            
+            $alias = $candidates[$random];
+            //print("\nSelecting: $alias");
+            
+            $response->selectProvider($alias);
+        }
+        catch (Exception $e) {
+            //print("\nException: $e");
+            $utilities->selectRandom();
+        }
+    }
+    
+    /**
+     * This method just wraps the PHP rand function, but provides
+     * a seam that can be used in unit testing.
+     */
+    public function rand($min, $max)
+    {
+    	return rand($min, $max);
+    }
+}
+
+?>
---
+++ apps/simple-geo/tests/OpenmixApplicationTests.php
@@ -0,0 +1,186 @@
+<?php
+
+require_once 'TestHelper.php';
+require_once(APP_DIR . '/OpenmixApplication.php');
+
+class OpenmixApplicationTests extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function init()
+    {
+        $config = $this->getMock('Configuration');
+        $application = new OpenmixApplication();
+        
+        $call_index = 0;
+        
+        $config->expects($this->at($call_index++))
+            ->method('declareInput')
+            ->with(GeoProperties::MARKET);
+        
+        // probe1.atl.tmd.prod,65.254.36.226
+        // probe1.rbx.ovh.prod,94.23.254.99
+        // probe1.sbg.ovh.prod,37.59.8.25
+        // probe1.sin.sl.prod,119.81.23.243
+        // probe1.sjc.sl.prod,50.97.227.68
+        
+        $config->expects($this->at($call_index++))
+            ->method('declareResponseOption')
+            ->with('probe1.atl.tmd.prod', '65.254.36.226', 20);
+            
+        $config->expects($this->at($call_index++))
+            ->method('declareResponseOption')
+            ->with('probe1.rbx.ovh.prod', '94.23.254.99', 20);
+            
+        $config->expects($this->at($call_index++))
+            ->method('declareResponseOption')
+            ->with('probe1.sbg.ovh.prod', '37.59.8.25', 20);
+            
+        $config->expects($this->at($call_index++))
+            ->method('declareResponseOption')
+            ->with('probe1.sin.sl.prod', '119.81.23.243', 20);
+            
+        $config->expects($this->at($call_index++))
+            ->method('declareResponseOption')
+            ->with('probe1.sjc.sl.prod', '50.97.227.68', 20);
+        
+        $application->init($config);
+    }
+    
+    /**
+     * @test
+     */
+    public function service()
+    {
+        $test_data = array(
+            // 'NA' => array( 'probe1.sjc.sl.prod', 'probe1.atl.tmd.prod' ),
+            array(
+                'market' => 'NA',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.sjc.sl.prod',
+            ),
+            array(
+                'market' => 'NA',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.atl.tmd.prod',
+            ),
+            // 'SA' => array( 'probe1.sjc.sl.prod', 'probe1.atl.tmd.prod' ),
+            array(
+                'market' => 'SA',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.sjc.sl.prod',
+            ),
+            array(
+                'market' => 'SA',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.atl.tmd.prod',
+            ),
+            // 'EU' => array( 'probe1.rbx.ovh.prod', 'probe1.sbg.ovh.prod' ),
+            array(
+                'market' => 'EU',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.rbx.ovh.prod',
+            ),
+            array(
+                'market' => 'EU',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.sbg.ovh.prod',
+            ),
+            // 'AF' => array( 'probe1.rbx.ovh.prod', 'probe1.sbg.ovh.prod' ),
+            array(
+                'market' => 'AF',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.rbx.ovh.prod',
+            ),
+            array(
+                'market' => 'AF',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.sbg.ovh.prod',
+            ),
+            // 'AS' => array( 'probe1.sin.sl.prod', 'probe1.sjc.sl.prod' ),
+            array(
+                'market' => 'AS',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.sin.sl.prod',
+            ),
+            array(
+                'market' => 'AS',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.sjc.sl.prod',
+            ),
+            // 'OC' => array( 'probe1.sin.sl.prod', 'probe1.sjc.sl.prod' ),
+            array(
+                'market' => 'OC',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.sin.sl.prod',
+            ),
+            array(
+                'market' => 'OC',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.sjc.sl.prod',
+            ),
+            // default, array( 'probe1.atl.tmd.prod', 'probe1.sbg.ovh.prod' )
+            array(
+                'market' => '',
+                'rand' => array( 0, 1, 0 ),
+                'alias' => 'probe1.atl.tmd.prod',
+            ),
+            array(
+                'market' => '',
+                'rand' => array( 0, 1, 1 ),
+                'alias' => 'probe1.sbg.ovh.prod',
+            ),
+        );
+        
+        foreach ($test_data as $i) {
+            $request = $this->getMock('Request');
+            $response = $this->getMock('Response');
+            $utilities = $this->getMock('Utilities');
+            $application = $this->getMock('OpenmixApplication', array('rand'));
+            
+            $request->expects($this->once())
+                ->method('geo')
+                ->with(GeoProperties::MARKET)
+                ->will($this->returnValue($i['market']));
+                
+            $application->expects($this->once())
+                ->method('rand')
+                ->with($i['rand'][0], $i['rand'][1])
+                ->will($this->returnValue($i['rand'][2]));
+                
+            
+            $response->expects($this->once())
+                ->method('selectProvider')
+                ->with($i['alias']);
+                
+            $utilities->expects($this->never())
+                ->method('selectRandom');
+            
+            $application->service($request, $response, $utilities);
+        }
+    }
+    
+    /**
+     * @test
+     */
+    public function service_exception_handling()
+    {
+        $request = $this->getMock('Request');
+        $response = $this->getMock('Response');
+        $utilities = $this->getMock('Utilities');
+        
+        $request->expects($this->once())
+            ->method('geo')
+            ->with(GeoProperties::MARKET)
+            ->will($this->throwException(new Exception('boo!!!')));
+            
+        $utilities->expects($this->once())
+            ->method('selectRandom');
+        
+        $application = new OpenmixApplication();
+        $application->service($request, $response, $utilities);
+    }
+}
+
+?>
