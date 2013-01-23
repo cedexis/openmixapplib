da7345af74d285c70b9a88cbf8ef1103795cbf95
Ben LeBlond <ben@cedexis.com>
2013-01-23 10:46:27 -0800

    [CEDEX-1716] Fixing phpunit test, Adding EDNS on/off use cases

--- apps/avoid-by-geo/app/OpenmixApplication.php
+++ apps/avoid-by-geo/app/OpenmixApplication.php
@@ -14,45 +14,41 @@ class OpenmixApplication implements Lifecycle
         'cdn1' => 'orig.example.cdn1.net',
         'cdn2' => 'www.example.com.cdn2.net',
         'cdn3' => 'www.someotherprovider.net');
-    
+
     private $reasons = array(
         'Best performing server selected' => 'A',
         'Data problem' => 'B',
         'All servers eliminated' => 'C');
-    
+
     private $ttl = 30;
-    
+
     private $availabilityThreshold = 90;
-    
+
     /**
      * @param Configuration $config
      */
     public function init($config)
     {
-
-        //$config->declareInput(EDNSProperties::ENABLE);
-        
         $config->declareInput(
             RadarProbeTypes::HTTP_RTT,
             implode(',', array_keys($this->servers)));
-        
+
         $config->declareInput(
             RadarProbeTypes::AVAILABILITY,
             implode(',', array_keys($this->servers)));
 
         $config->declareInput(GeoProperties::COUNTRY);
+        $config->declareInput(EDNSProperties::COUNTRY);
 
-        foreach ($this->servers as $alias => $cname)
-        {
+        foreach ($this->servers as $alias => $cname) {
             $config->declareResponseOption($alias, $cname, $this->ttl);
         }
-        
-        foreach ($this->reasons as $code)
-        {
+
+        foreach ($this->reasons as $code) {
             $config->declareReasonCode($code);
         }
     }
-    
+
     /**
      * @param Request $request
      * @param Response $response
@@ -60,43 +56,40 @@ class OpenmixApplication implements Lifecycle
      */
     public function service($request, $response, $utilities)
     {
-        $rtt = $request->radar(RadarProbeTypes::HTTP_RTT);
 
-        $country = $request->geo(GeoProperties::COUNTRY);
+        $rtt = $request->radar(RadarProbeTypes::HTTP_RTT);
+        $edns_enabled = $request->geo(EDNSProperties::ENABLE);
+
+        if ($edns_enabled) {
+            $country = $request->geo(EDNSProperties::COUNTRY);
+        } else {
+            $country = $request->geo(GeoProperties::COUNTRY);
+        }
 
         // Only consider cdn2 for requests originating from China
-        if ( $country != 'CN' )
-        {
+        if ($country != 'CN') {
             unset($rtt["cdn2"]);
         }
-        
-        if (is_array($rtt) && (0 < count($rtt)))
-        {
+
+        if (is_array($rtt) && (0 < count($rtt))) {
             $candidates = array_intersect_key($rtt, $this->servers);
-            if (0 < count($candidates))
-            {
+            if (0 < count($candidates)) {
                 // Select the best performing server that meets its minimum
                 // availability score, if given
                 asort($candidates);
                 $avail = $request->radar(RadarProbeTypes::AVAILABILITY);
-                foreach (array_keys($candidates) as $alias)
-                {
-                    if ($avail[$alias] >= $this->availabilityThreshold)
-                    {
+                foreach (array_keys($candidates) as $alias) {
+                    if ($avail[$alias] >= $this->availabilityThreshold) {
                         $response->selectProvider($alias);
                         $response->setReasonCode($this->reasons['Best performing server selected']);
                         return;
                     }
                 }
                 $response->setReasonCode($this->reasons['All servers eliminated']);
-            }
-            else
-            {
+            } else {
                 $response->setReasonCode($this->reasons['Data problem']);
             }
-        }
-        else
-        {
+        } else {
             $response->setReasonCode($this->reasons['Data problem']);
         }
         $utilities->selectRandom();
--- apps/avoid-by-geo/tests/OpenmixApplicationTests.php
+++ apps/avoid-by-geo/tests/OpenmixApplicationTests.php
@@ -34,21 +34,24 @@ class OpenmixTest extends PHPUnit_Framework_TestCase
             ->method('declareInput')
             ->with(GeoProperties::COUNTRY);
 
-        foreach ($servers as $alias => $cname)
-        {
+        $config->expects($this->at($callIndex++))
+            ->method('declareInput')
+            ->with(EDNSProperties::COUNTRY);
+
+        foreach ($servers as $alias => $cname) {
             $config->expects($this->at($callIndex++))
                 ->method('declareResponseOption')
                 ->with($alias, $cname, 30);
         }
 
-        foreach ($reasons as $code)
-        {
+        foreach ($reasons as $code) {
             $config->expects($this->at($callIndex++))
                 ->method('declareReasonCode')
                 ->with($code);
         }
-        
+
         $app = new OpenmixApplication();
+
         $app->init($config);
     }
 
@@ -61,7 +64,8 @@ class OpenmixTest extends PHPUnit_Framework_TestCase
             // Don't select cdn2 even though it's fastest because the request is not from China.
             array(
                 'rtt' => array('cdn1' => 200, 'cdn2' => 199, 'cdn3' => 200),
-                'country' => 'US',
+                'edns_country' => 'US',
+                'geo_country' => 'US',
                 'avail' => array('cdn1' => 90, 'cdn2' => 90, 'cdn3' => 90),
                 'alias' => 'cdn3',
                 'reason' => 'A'),
@@ -69,48 +73,67 @@ class OpenmixTest extends PHPUnit_Framework_TestCase
             // is not from China. Select cdn1 because it's next fastest.
             array(
                 'rtt' => array('cdn1' => 200, 'cdn2' => 199, 'cdn3' => 201),
-                'country' => 'GB',
+                'edns_country' => 'GB',
+                'geo_country' => 'GB',
                 'avail' => array('cdn1' => 90, 'cdn2' => 90, 'cdn3' => 90),
                 'alias' => 'cdn1',
                 'reason' => 'A'),
             // Select cdn2 because it's fastest, and the request is from China.
             array(
                 'rtt' => array('cdn1' => 200, 'cdn2' => 199, 'cdn3' => 200),
-                'country' => 'CN',
+                'edns_country' => 'CN',
+                'geo_country' => 'CN',
                 'avail' => array('cdn1' => 90, 'cdn2' => 90, 'cdn3' => 90),
                 'alias' => 'cdn2',
                 'reason' => 'A'),
             // All servers are below availability threshold.
             array(
                 'rtt' => array('cdn1' => 200, 'cdn2' => 200, 'cdn3' => 200),
-                'country' => 'CN',
+                'edns_country' => 'CN',
+                'geo_country' => 'CN',
                 'avail' => array('cdn1' => 89, 'cdn2' => 89, 'cdn3' => 89),
                 'reason' => 'C'),
             // Missing data
             array(
                 'rtt' => null,
-                'country' => 'some country',
+                'edns_country' => 'some country',
+                'geo_country' => 'some country',
                 'reason' => 'B'),
             array(
                 'rtt' => null,
-                'country' => 'CN',
+                'edns_country' => 'CN',
+                'geo_country' => 'CN',
                 'reason' => 'B'),
             array(
                 'rtt' => array(),
-                'country' => 'some country',
+                'edns_country' => 'some country',
+                'geo_country' => 'some country',
+                'reason' => 'B'),
+            array(
+                // edns on
+                'rtt' => array(),
+                'edns_country' => 'FR',
+                'geo_country' => 'US',
+                'reason' => 'B'),
+            array(
+                // edns off
+                'edns' => 0,
+                'rtt' => array(),
+                'edns_country' => 'FR',
+                'geo_country' => 'US',
                 'reason' => 'B'),
             array(
                 // China data missing
                 'rtt' => array('cdn1' => 200, 'cdn3' => 201),
                 'avail' => array('cdn1' => 90, 'cdn2' => 90, 'cdn3' => 90),
-                'country' => 'CN',
+                'edns_country' => 'CN',
+                'geo_country' => 'CN',
                 'alias' => 'cdn1',
                 'reason' => 'A'),
         );
-        
+
         $testIndex = 0;
-        foreach ($testData as $i)
-        {
+        foreach ($testData as $i) {
 
             $request = $this->getMock('Request');
             $response = $this->getMock('Response');
@@ -122,46 +145,60 @@ class OpenmixTest extends PHPUnit_Framework_TestCase
                 ->method('radar')
                 ->with(RadarProbeTypes::HTTP_RTT)
                 ->will($this->returnValue($i['rtt']));
-                
+
+            $edns_enabled = 1;
+            if (array_key_exists('edns', $i) && $i['edns'] == 0) {
+                $edns_enabled = 0;
+            }
+
             $request->expects($this->at($reqCallIndex++))
                 ->method('geo')
-                ->with(GeoProperties::COUNTRY)
-                ->will($this->returnValue($i['country']));
-                
-            if (array_key_exists('avail', $i))
-            {
+                ->with(EDNSProperties::ENABLE)
+                ->will($this->returnValue($edns_enabled));
+
+            // if EDNS is turned off, we'll use GEO
+            if ($edns_enabled) {
+                $request->expects($this->at($reqCallIndex++))
+                    ->method('geo')
+                    ->with(EDNSProperties::COUNTRY)
+                    ->will($this->returnValue($i['edns_country']));
+            } else {
+                $request->expects($this->at($reqCallIndex++))
+                    ->method('geo')
+                    ->with(GeoProperties::COUNTRY)
+                    ->will($this->returnValue($i['geo_country']));
+            }
+
+            if (array_key_exists('avail', $i)) {
                 $request->expects($this->at($reqCallIndex++))
                     ->method('radar')
                     ->with(RadarProbeTypes::AVAILABILITY)
                     ->will($this->returnValue($i['avail']));
             }
-            
-            if (array_key_exists('alias', $i))
-            {
+
+            if (array_key_exists('alias', $i)) {
                 $response->expects($this->once())
                     ->method('selectProvider')
                     ->with($i['alias']);
-                    
+
                 $utilities->expects($this->never())
                     ->method('selectRandom');
-            }
-            else
-            {
+            } else {
                 $utilities->expects($this->once())
                     ->method('selectRandom');
-                    
+
                 $response->expects($this->never())
                     ->method('selectProvider');
             }
-            
+
             $response->expects($this->once())
                 ->method('setReasonCode')
                 ->with($i['reason']);
-                
+
             $app = new OpenmixApplication();
             $app->service($request, $response, $utilities);
             $this->verifyMockObjects();
-            print("\nTest index: " . $testIndex++."\n");
+            print("\nTest index: " . $testIndex++ . "\n");
         }
     }
 }
