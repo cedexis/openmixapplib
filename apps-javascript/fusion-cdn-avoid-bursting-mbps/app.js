0068f81fbc47c309015a078bc9dfd3a19b9e28cf
Jacob Wan <jacob@wildlemur.com>
2014-09-23 16:04:55 -0700

    Add Fusion MBPS template

---
+++ apps-javascript/fusion-cdn-avoid-bursting-mbps/app.js
@@ -0,0 +1,270 @@
+
+var handler;
+
+/** @constructor */
+function OpenmixApplication(settings) {
+    'use strict';
+
+    var reasons = {
+        best_performing: 'A',
+        data_problem: 'B',
+        all_providers_eliminated: 'C',
+        missing_bandwidth_data: 'D',
+        radar_data_sparse: 'E',
+        missing_fusion_data: 'F'
+    };
+
+    function sort_thresholds_in_descending_order(left, right) {
+        if (left.threshold > right.threshold) {
+            return -1;
+        }
+        if (left.threshold < right.threshold) {
+            return 1;
+        }
+        return 0;
+    }
+
+    (function() {
+        // Sort the burstable thresholds in descending order
+        var i;
+        for (i in settings.burstable_cdns) {
+            if (settings.burstable_cdns.hasOwnProperty(i)) {
+                settings.burstable_cdns[i].bandwidth.sort(sort_thresholds_in_descending_order);
+            }
+        }
+    }());
+    //console.log(JSON.stringify(settings.burstable_cdns));
+
+    this.get_random = function() {
+        return Math.random();
+    };
+
+    this.get_random_int = function(min, max) {
+        return Math.floor(this.get_random() * (max - min)) + min;
+    };
+
+    this.select_random_provider = function() {
+        return settings.providers[this.get_random_int(0, settings.providers.length - 1)];
+    };
+
+    /** @param {OpenmixConfiguration} config */
+    this.do_init = function(config) {
+        var i;
+        for (i = 0; i < settings.providers.length; i += 1) {
+            config.requireProvider(settings.providers[i].alias);
+        }
+    };
+
+    /**
+     * @param {OpenmixRequest} request
+     * @param {OpenmixResponse} response
+     */
+    this.handle_request = function(request, response) {
+
+        var data = {},
+            padded_rtt,
+            decision_provider,
+            decision_ttl = settings.default_ttl,
+            decision_reasons = [];
+
+        function flatten(obj, property) {
+            var result = {}, i;
+            for (i in obj) {
+                if (obj.hasOwnProperty(i)) {
+                    if (obj[i].hasOwnProperty(property) && obj[i][property]) {
+                        result[i] = obj[i][property];
+                    }
+                }
+            }
+            return result;
+        }
+
+        function provider_from_alias(alias) {
+            var i;
+            for (i = 0; i < settings.providers.length; i += 1) {
+                if (alias === settings.providers[i].alias) {
+                    return settings.providers[i];
+                }
+            }
+            return null;
+        }
+
+        function parse_fusion_data(source) {
+            var result = {}, i;
+            for (i in source) {
+                if (source.hasOwnProperty(i)) {
+                    try {
+                        result[i] = JSON.parse(source[i]);
+                    } catch (ignore) {
+                        // Swallow the exception
+                    }
+                }
+            }
+            return result;
+        }
+
+        function filter_invalid_rtt_scores(data) {
+            var result = {}, i;
+            for (i in data) {
+                if (data.hasOwnProperty(i)) {
+                    if (settings.min_valid_rtt_score <= data[i]) {
+                        result[i] = data[i];
+                    }
+                }
+            }
+            return result;
+        }
+
+        function add_rtt_padding(data) {
+            var result = {}, i, j, bandwidth_value, current, padding_pct, padding, hit_threshold;
+            for (i in data.rtt) {
+                if (data.rtt.hasOwnProperty(i)) {
+                    if (settings.burstable_cdns[i]) {
+                        //console.log(data.fusion[i]);
+                        //console.log(data.rtt[i]);
+
+                        if (data.fusion[i] && data.fusion[i].bandwidth && data.fusion[i].bandwidth.value && settings.burstable_cdns[i].bandwidth) {
+                            bandwidth_value = parseFloat(data.fusion[i].bandwidth.value);
+
+                            hit_threshold = false;
+                            for (j = 0; !hit_threshold && (j < settings.burstable_cdns[i].bandwidth.length); j += 1) {
+                                current = settings.burstable_cdns[i].bandwidth[j];
+                                if (bandwidth_value >= current.threshold) {
+                                    hit_threshold = true;
+                                    break;
+                                }
+                            }
+
+                            if (hit_threshold) {
+                                //console.log('hit threshold: ' + JSON.stringify(current));
+                                padding_pct = (bandwidth_value / current.threshold - 1) * current.multiplier * 100;
+                                //console.log('padding_pct: ' + padding_pct);
+                                padding = 1 + padding_pct / 100;
+                                //console.log('padding: ' + padding);
+                                result[i] = padding * data.rtt[i];
+                            } else {
+                                result[i] = data.rtt[i];
+                            }
+                        } else {
+                            // No padding, but note the reason
+                            result[i] = data.rtt[i];
+                            if (-1 === decision_reasons.indexOf(reasons.missing_bandwidth_data)) {
+                                decision_reasons.push(reasons.missing_bandwidth_data);
+                            }
+                        }
+                    } else {
+                        // Not concerned about bursting for this provider
+                        result[i] = data.rtt[i];
+                    }
+                }
+            }
+            return result;
+        }
+
+        function as_tuples(obj) {
+            var i, result = [];
+            for (i in obj) {
+                if (obj.hasOwnProperty(i)) {
+                    result.push([i, obj[i]]);
+                }
+            }
+            return result;
+        }
+
+        // Application logic here
+        data.avail = flatten(request.getProbe('avail'), 'avail');
+        data.rtt = filter_invalid_rtt_scores(flatten(request.getProbe('http_rtt'), 'http_rtt'));
+        data.fusion = parse_fusion_data(request.getData('fusion'));
+        //console.log(JSON.stringify(data));
+
+        if ((Object.keys(data.rtt).length !== settings.providers.length) || (Object.keys(data.avail).length !== settings.providers.length)) {
+            decision_provider = this.select_random_provider();
+            decision_reasons.push(reasons.radar_data_sparse);
+            decision_ttl = settings.error_ttl;
+        } else if (Object.keys(data.fusion).length !== Object.keys(settings.burstable_cdns).length) {
+            decision_provider = this.select_random_provider();
+            decision_reasons.push(reasons.missing_fusion_data);
+            decision_ttl = settings.error_ttl;
+        } else {
+            data.rtt = (function() {
+                var result = {}, i;
+                for (i in data.rtt) {
+                    if (data.rtt.hasOwnProperty(i) && data.avail[i]) {
+                        if (data.avail[i] >= settings.availability_threshold) {
+                            result[i] = data.rtt[i];
+                        }
+                    }
+                }
+                return result;
+            }());
+            padded_rtt = as_tuples(add_rtt_padding(data));
+            //console.log('rtt (padded, as tuples): ' + JSON.stringify(padded_rtt));
+            padded_rtt.sort(function(left, right) {
+                if (left[1] < right[1]) {
+                    return -1;
+                }
+                if (left[1] > right[1]) {
+                    return 1;
+                }
+                return 0;
+            });
+            //console.log('rtt (sorted): ' + JSON.stringify(padded_rtt));
+            decision_provider = provider_from_alias(padded_rtt[0][0]);
+            decision_reasons.push(reasons.best_performing);
+        }
+
+        response.respond(decision_provider.alias, decision_provider.cname);
+        response.setTTL(decision_ttl);
+        response.setReasonCode(decision_reasons.join(','));
+    };
+
+}
+
+handler = new OpenmixApplication({
+    providers: [
+        {
+            alias: 'foo',
+            cname: 'www.foo.com',
+            base_padding: 0
+        },
+        {
+            alias: 'bar',
+            cname: 'www.bar.com',
+            base_padding: 0
+        },
+        {
+            alias: 'baz',
+            cname: 'www.baz.com'
+        }
+    ],
+    burstable_cdns: {
+        'foo': {
+            bandwidth: [
+                { threshold: 10000, multiplier: 1.2 },
+                { threshold: 7500, multiplier: 1.3 },
+                { threshold: 5000, multiplier: 1.5 }
+            ]
+        },
+        'bar': {
+            bandwidth: [
+                { threshold: 10000, multiplier: 1.2 },
+                { threshold: 7500, multiplier: 1.3 },
+                { threshold: 5000, multiplier: 1.5 }
+            ]
+        }
+    },
+    default_ttl: 20,
+    error_ttl: 20,
+    min_valid_rtt_score: 5,
+    availability_threshold: 90
+});
+
+function init(config) {
+    'use strict';
+    handler.do_init(config);
+}
+
+function onRequest(request, response) {
+    'use strict';
+    handler.handle_request(request, response);
+}
---
+++ apps-javascript/fusion-cdn-avoid-bursting-mbps/test/tests.js
@@ -0,0 +1,520 @@
+/*global
+    module,
+    test,
+    equal,
+    deepEqual,
+    OpenmixApplication,
+    init,
+    onRequest,
+    console,
+*/
+
+(function() {
+    'use strict';
+
+    var default_settings = {
+        providers: [
+            {
+                alias: 'foo',
+                cname: 'www.foo.com',
+                base_padding: 0
+            },
+            {
+                alias: 'bar',
+                cname: 'www.bar.com',
+                base_padding: 0
+            },
+            {
+                alias: 'baz',
+                cname: 'www.baz.com'
+            }
+        ],
+        burstable_cdns: {
+            'foo': {
+                bandwidth: [
+                    { threshold: 10000, multiplier: 1.2 },
+                    { threshold: 7500, multiplier: 1.3 },
+                    { threshold: 5000, multiplier: 1.5 }
+                ]
+            },
+            'bar': {
+                bandwidth: [
+                    { threshold: 10000, multiplier: 1.2 },
+                    { threshold: 7500, multiplier: 1.3 },
+                    { threshold: 5000, multiplier: 1.5 }
+                ]
+            }
+        },
+        default_ttl: 20,
+        error_ttl: 10,
+        min_valid_rtt_score: 5,
+        availability_threshold: 90
+    };
+
+    module('do_init');
+
+    function test_do_init(i) {
+        return function() {
+
+            var sut,
+                config = {
+                    requireProvider: this.stub()
+                },
+                test_stuff = {
+                    config: config
+                };
+
+            i.setup(test_stuff);
+
+            sut = new OpenmixApplication(i.settings || default_settings);
+
+            // Test
+            sut.do_init(config);
+
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('default', test_do_init({
+        setup: function() { return; },
+        verify: function(i) {
+            equal(i.config.requireProvider.callCount, 3, 'Verifying requireProvider call count');
+            equal(i.config.requireProvider.args[0][0], 'foo', 'Verirying provider alias');
+            equal(i.config.requireProvider.args[1][0], 'bar', 'Verirying provider alias');
+            equal(i.config.requireProvider.args[2][0], 'baz', 'Verirying provider alias');
+        }
+    }));
+
+    module('handle_request');
+
+    function test_handle_request(i) {
+        return function() {
+            var sut,
+                request = {
+                    getData: this.stub(),
+                    getProbe: this.stub()
+                },
+                response = {
+                    respond: this.stub(),
+                    setTTL: this.stub(),
+                    setReasonCode: this.stub()
+                },
+                test_stuff;
+
+            sut = new OpenmixApplication(i.settings || default_settings);
+            this.stub(sut, 'get_random');
+
+            test_stuff = {
+                request: request,
+                response: response,
+                sut: sut
+            };
+
+            i.setup(test_stuff);
+
+            // Test
+            sut.handle_request(request, response);
+
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('foo faster; bar fastest after padding', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 100
+                    },
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 200
+                    },
+                    "bar": {
+                        "http_rtt": 201
+                    },
+                    "baz": {
+                        "http_rtt": 220
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "5001.31"
+                        }
+                    })
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'bar', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.bar.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('foo fastest after padding', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 100
+                    },
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 155
+                    },
+                    "bar": {
+                        "http_rtt": 201
+                    },
+                    "baz": {
+                        "http_rtt": 220
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "6112.31"
+                        }
+                    })
+                });
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('foo fastest available', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 90
+                    },
+                    "bar": {
+                        "avail": 89
+                    },
+                    "baz": {
+                        "avail": 90
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 200
+                    },
+                    "bar": {
+                        "http_rtt": 100
+                    },
+                    "baz": {
+                        "http_rtt": 300
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "6112.31"
+                        }
+                    })
+                });
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('missing avail data for foo', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {},
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 155
+                    },
+                    "bar": {
+                        "http_rtt": 201
+                    },
+                    "baz": {
+                        "http_rtt": 220
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "6112.31"
+                        }
+                    })
+                });
+            i.sut.get_random.returns(0);
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 10, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'E', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('missing rtt data for baz', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 100
+                    },
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 155
+                    },
+                    "bar": {
+                        "http_rtt": 201
+                    },
+                    "baz": {}
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "6112.31"
+                        }
+                    })
+                });
+            i.sut.get_random.returns(0);
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 10, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'E', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('invalid rtt score for baz', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 100
+                    },
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 155
+                    },
+                    "bar": {
+                        "http_rtt": 201
+                    },
+                    "baz": {
+                        "http_rtt": 4
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "6112.31"
+                        }
+                    })
+                });
+            i.sut.get_random.returns(0);
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 10, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'E', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('missing fusion data for bar', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    "foo": {
+                        "avail": 100
+                    },
+                    "bar": {
+                        "avail": 100
+                    },
+                    "baz": {
+                        "avail": 100
+                    }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    "foo": {
+                        "http_rtt": 200
+                    },
+                    "bar": {
+                        "http_rtt": 200
+                    },
+                    "baz": {
+                        "http_rtt": 200
+                    }
+                });
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": JSON.stringify({
+                        "bandwidth": {
+                            "unit": "Mbps",
+                            "value": "8623.57"
+                        }
+                    }),
+                    "bar": ""
+                });
+            i.sut.get_random.returns(0);
+        },
+        verify: function(i) {
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 10, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'F', 'Verifying setReasonCode');
+        }
+    }));
+
+}());
