ab14bb0071ebc7f078a9cfa282498799cf2f2931
Damian Mansilla <damian@cedexis.com>
2014-10-14 16:32:57 -0300

    round-robin-failover

---
+++ apps-javascript/round-robin-failover/app.js
@@ -0,0 +1,185 @@
+var handler = new OpenmixApplication({
+    // `providers` contains a list of the providers to be load-balanced
+    // `alias` is the Openmix alias set in the Portal
+    // `cname` is the CNAME or IP address to be sent as the answer when this provider is selected
+    providers: {
+        'foo': {
+            cname: 'www.foo.com'
+        },
+        'bar': {
+            cname: 'www.bar.com'
+        }
+    },
+    failover_providers: {
+        'foo_f': {
+            cname: 'www.foo_f.com'
+        },
+        'bar_f': {
+            cname: 'www.bar_f.com'
+        },
+        'baz_f': {
+            cname: 'www.baz_f.com'
+        }
+    },
+    default_provider: 'foo',
+    // The TTL to be set when the application chooses a geo provider.
+    default_ttl: 20,
+    sonar_threshold: 90
+});
+
+function init(config) {
+    'use strict';
+    handler.do_init(config);
+}
+
+function onRequest(request, response) {
+    'use strict';
+    handler.handle_request(request, response);
+}
+
+/** @constructor */
+function OpenmixApplication(settings) {
+    'use strict';
+
+    var aliases = typeof settings.providers === 'undefined' ? [] : Object.keys(settings.providers);
+    var failover_aliases = typeof settings.failover_providers === 'undefined' ? [] : Object.keys(settings.failover_providers);
+
+    /**
+     * @param {OpenmixConfiguration} config
+     */
+    this.do_init = function(config) {
+        var i = aliases.length;
+
+        while (i --) {
+            config.requireProvider(aliases[i]);
+        }
+
+        i = failover_aliases.length;
+
+        while (i --) {
+            config.requireProvider(failover_aliases[i]);
+        }
+
+    };
+
+    /**
+     * @param {OpenmixRequest} request
+     * @param {OpenmixResponse} response
+     */
+    this.handle_request = function(request, response) {
+        var data_sonar  = parse_sonar_data(request.getData('sonar')),
+            all_reasons,
+            decision_provider,
+            decision_ttl,
+            candidates,
+            candidate_aliases,
+            reason_code,
+            decision_cname;
+
+        all_reasons = {
+            primary_selected: 'A',
+            failover_selected: 'B',
+            default_selected: 'C'
+        };
+
+        function filter_primary_candidates(candidate, alias) {
+            return (candidate >= settings.sonar_threshold)
+                && (typeof settings.providers[alias] !== 'undefined');
+        }
+
+        function filter_failover_candidates(candidate, alias) {
+            return (candidate >= settings.sonar_threshold)
+                && (typeof settings.failover_providers[alias] !== 'undefined');
+        }
+
+        function select_random_provider(aliases, reason) {
+            decision_provider = aliases[Math.floor(Math.random() * aliases.length)];
+            reason_code = reason;
+            decision_ttl = settings.default_ttl;
+        }
+
+        candidates = filter_object(data_sonar, filter_primary_candidates);
+
+        candidate_aliases = Object.keys(candidates);
+
+        if (candidate_aliases.length === 1) {
+            decision_provider = candidate_aliases[0];
+            reason_code = all_reasons.primary_selected;
+            decision_ttl = decision_ttl || settings.default_ttl;
+        } else if (candidate_aliases.length !== 0) {
+            select_random_provider(aliases, all_reasons.primary_selected);
+        }
+
+        if (!decision_provider) {
+            candidates = filter_object(data_sonar, filter_failover_candidates);
+
+            candidate_aliases = Object.keys(candidates);
+
+            if (candidate_aliases.length === 1) {
+                decision_provider = candidate_aliases[0];
+                decision_cname = settings.failover_providers[decision_provider].cname;
+                reason_code = all_reasons.failover_selected;
+                decision_ttl = decision_ttl || settings.default_ttl;
+            } else if (candidate_aliases.length !== 0) {
+                select_random_provider(failover_aliases, all_reasons.failover_selected);
+                decision_cname = settings.failover_providers[decision_provider].cname;
+            }
+        }
+
+        if (!decision_provider) {
+            decision_provider = settings.default_provider;
+            reason_code = all_reasons.default_selected;
+            decision_ttl = decision_ttl || settings.default_ttl;
+        }
+
+        response.respond(decision_provider, decision_cname || settings.providers[decision_provider].cname);
+        response.setTTL(decision_ttl);
+        response.setReasonCode(reason_code);
+    };
+
+    /**
+     * @param {Object} object
+     * @param {Function} filter
+     */
+    function filter_object(object, filter) {
+        var keys = Object.keys(object),
+            i = keys.length,
+            key,
+            data = [];
+
+        while (i --) {
+            key = keys[i];
+
+            if (filter(object[key], key)) {
+                data[key] = (object[key]);
+            }
+            /*
+            if (!filter(object[key], key)) {
+                delete object[key];
+            }
+            */
+        }
+
+        //return object;
+        return data;
+    }
+
+    /**
+     * @param {string} data
+     */
+    function parse_sonar_data(data) {
+        var keys = Object.keys(data),
+            i = keys.length,
+            key;
+        while (i --) {
+            key = keys[i];
+            try {
+                data[key] = JSON.parse(data[key]);
+            }
+            catch (e) {
+                delete data[key];
+            }
+        }
+        return data;
+    }
+}
---
+++ apps-javascript/round-robin-failover/test/tests.js
@@ -0,0 +1,289 @@
+/*global
+    module,
+    test,
+    equal,
+    deepEqual,
+    OpenmixApplication,
+    init,
+    onRequest,
+    console,
+*/
+
+(function() {
+    'use strict';
+
+    var default_settings = {
+        providers: {
+            'foo': {
+                cname: 'www.foo.com'
+            },
+            'bar': {
+                cname: 'www.bar.com'
+            }
+        },
+        failover_providers: {
+            'foo_f': {
+                cname: 'www.foo_f.com'
+            },
+            'bar_f': {
+                cname: 'www.bar_f.com'
+            }
+        },
+        default_provider: 'foo',
+        default_ttl: 20,
+        sonar_threshold: 90
+    };
+
+    module('do_init');
+
+    function test_do_init(i) {
+        return function() {
+
+            var sut,
+                config = {
+                    requireProvider: this.stub()
+                },
+                test_stuff = {
+                    config: config
+                };
+
+            i.setup(test_stuff);
+
+            sut = new OpenmixApplication(i.settings || default_settings);
+
+            // Test
+            sut.do_init(config);
+
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('default', test_do_init({
+        setup: function() { return; },
+        verify: function(i) {
+            equal(i.config.requireProvider.callCount, 4, 'Verifying requireProvider call count');
+            equal(i.config.requireProvider.args[3][0], 'foo_f', 'Verirying failover provider alias');
+            equal(i.config.requireProvider.args[2][0], 'bar_f', 'Verirying failover provider alias');
+            equal(i.config.requireProvider.args[1][0], 'foo', 'Verirying provider alias');
+            equal(i.config.requireProvider.args[0][0], 'bar', 'Verirying provider alias');
+        }
+    }));
+
+    module('handle_request');
+
+    function test_handle_request(i) {
+        return function() {
+            var sut,
+                config = {
+                    requireProvider: this.stub()
+                },
+                request = {
+                    getData: this.stub(),
+                    getProbe: this.stub()
+                },
+                response = {
+                    respond: this.stub(),
+                    setTTL: this.stub(),
+                    setReasonCode: this.stub()
+                },
+                test_stuff;
+
+            sut = new OpenmixApplication(i.settings || default_settings);
+            sut.do_init(config);
+
+            this.stub(Math, 'random');
+            Math.random.returns(0);
+
+            test_stuff = {
+                request: request,
+                response: response,
+                sut: sut
+            };
+
+            i.setup(test_stuff);
+
+            // Test
+            sut.handle_request(request, response);
+
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('test 1', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 100,
+                    "bar": 100,
+                    "foo_f": 100,
+                    "bar_f": 100
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 2', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 88,
+                    "bar": 90,
+                    "foo_f": 100,
+                    "bar_f": 100
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'bar', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.bar.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 3', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 90,
+                    "bar": 89,
+                    "foo_f": 100,
+                    "bar_f": 100
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 4', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 89,
+                    "bar": 89,
+                    "foo_f": 100,
+                    "bar_f": 100
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo_f', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo_f.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'B', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 5', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 89,
+                    "bar": 89,
+                    "foo_f": 90,
+                    "bar_f": 89
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo_f', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo_f.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'B', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 6', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 89,
+                    "bar": 89,
+                    "foo_f": 89,
+                    "bar_f": 90
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'bar_f', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.bar_f.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'B', 'Verifying setReasonCode');
+        }
+    }));
+
+    test('test 7', test_handle_request({
+        setup: function(i) {
+            i.request
+                .getData
+                .onCall(0)
+                .returns({
+                    "foo": 89,
+                    "bar": 89,
+                    "foo_f": 89,
+                    "bar_f": 89
+                });
+        },
+        verify: function(i) {
+            equal(i.request.getData.callCount, 1, 'Verifying getData call count');
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying respond provider');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying respond CNAME');
+            equal(i.response.setTTL.args[0][0], 20, 'Verifying setTTL');
+            equal(i.response.setReasonCode.args[0][0], 'C', 'Verifying setReasonCode');
+        }
+    }));
+
+}());
