8381176b0fd81f41e58d4a865c0fe6fdddfee95e
Gaston <gaston@cedexis.com>
2014-11-20 14:10:18 -0300

    CEDEX-7370_sticky-perf-with-penalty-and-availability-PHP-app-template-to-JS

---
+++ apps-javascript/sticky-perf-with-penalty-and-availability/app.js
@@ -0,0 +1,308 @@
+var handler = new OpenmixApplication({
+    // `providers` contains a list of the providers to be load-balanced
+    // `alias` is the Openmix alias set in the Portal
+    // `cname` is the CNAME or IP address to be sent as the answer when this provider is selected
+    providers: {
+        'foo': {
+            cname: 'www.foo.com',
+            padding:0
+        },
+        'bar': {
+            cname: 'www.bar.com',
+            padding:10
+        },
+        'baz': {
+            cname: 'www.baz.com',
+            padding:0
+        }
+    },
+    // Selected if a provider can't be determined
+    default_provider: 'foo',
+
+    sticky_countries: [],
+    // The TTL to be set when the application chooses a geo provider.
+    default_ttl: 30,
+    // The TTL to be set when the application chooses the default provider.
+    error_ttl: 20,
+    availability_threshold: 60,
+    variance_threshold: 0.65,
+
+    /**
+     * @type {number}
+     */
+    maxSavedProviders: 800
+});
+
+function init(config) {
+    'use strict';
+    handler.do_init(config);
+}
+
+function onRequest(request, response) {
+    'use strict';
+    handler.handle_request(request, response);
+}
+
+/** @constructor */
+function OpenmixApplication(settings) {
+    'use strict';
+
+    var aliases = typeof settings.providers === 'undefined' ? [] : Object.keys(settings.providers);
+    this.lastAliasIndex = -1;
+
+    /**
+     * @type (Object) An object storing the last provider selected for a
+     * particular market/country combination.  The object is keyed by
+     * "<market>-<country>".
+     */
+    this.saved = {};
+
+    /**
+     * @param {OpenmixConfiguration} config
+     */
+    this.do_init = function(config) {
+        var i = aliases.length;
+
+        while (i --) {
+            config.requireProvider(aliases[i]);
+        }
+    };
+
+    /**
+     * @param {OpenmixRequest} request
+     * @param {OpenmixResponse} response
+     */
+    this.handle_request = function(request, response) {
+        var avail = request.getProbe('avail'),
+            rtt = request.getProbe('http_rtt'),
+            allReasons,
+            decisionProvider,
+            decisionReason,
+            /**
+             * An object containing the list of candidate providers, keyed by
+             * provider alias.  It is composed by joining the Radar availability
+             * and HTTP RTT data taken from the Openmix API.  It is also run
+             * through a filter to remove unavailable providers.
+             *
+             * Example after composition:
+             *
+             * {
+             *     "foo": {
+             *         "avail": 99,
+             *         "http_rtt": 201
+             *     }
+             *     "bar": {
+             *         "avail": 100,
+             *         "http_rtt": 198
+             *     }
+             * }
+             *
+             * @type (Object.<string,{http_rtt:number,avail:number}>)
+             */
+            candidates,
+
+            candidateAliases,
+            cnameOverride = '',
+            stickyKey = request.market + "-" + request.country + "-" + request.asn,
+            previous,
+            refValue;
+
+        allReasons = {
+            best_performing_provider_equal_previous: 'A',
+            no_previous: 'B',
+            previous_below_availability_threshold: 'C',
+            new_provider_below_varianceThreshold: 'D',
+            choosing_previous_best_perform_within_varianceThreshold: 'E',
+            all_providers_eliminated: 'F',
+            unexpected_previous_alias: 'G'
+        };
+
+        function filterCandidates(candidate) {
+            return (typeof candidate.avail !== 'undefined' && candidate.avail >= settings.availability_threshold);
+        }
+
+        //update_sticky_data
+
+        var filtered = settings.sticky_countries[request.country];
+        if (typeof filtered !== 'undefined' || settings.sticky_countries.length === 0) {
+            if (typeof this.saved[stickyKey] === 'undefined') {
+                while (settings.maxSavedProviders <= Object.keys(this.saved).length) {
+                    delete this.saved[getLRUIndex(this.saved)];
+                }
+                this.saved[stickyKey] = {'provider': null}; // saves the stickyKey with a provider null
+            }
+            this.saved[stickyKey].timestamp = new Date().getTime();
+        }
+
+        previous = null;
+
+        if (typeof this.saved[stickyKey] !== 'undefined') {
+            previous = this.saved[stickyKey].provider;
+        }
+
+        if(previous !== null && typeof settings.providers[previous] === 'undefined'){
+            decisionProvider =  aliases[Math.floor(Math.random() * aliases.length)];
+            decisionReason = allReasons.unexpected_previous_alias;
+        }
+        else {
+
+            candidates = filterObject(avail, filterCandidates);
+
+            // Join the rtt scores with the list of viable candidates
+            candidates = joinObjects(candidates, rtt, 'http_rtt');
+
+            candidateAliases = Object.keys(candidates);
+
+
+            if (candidateAliases.length !== 0) {
+                add_rtt_padding(candidates);
+                if (typeof avail[previous] !== 'undefined') {
+                    refValue = settings.variance_threshold * avail[previous].http_rtt;
+                }
+                var bestRtt = getLowest(candidates, 'http_rtt');
+                decisionProvider = bestRtt;
+
+                if (bestRtt === previous) {
+                    decisionReason = allReasons.best_performing_provider_equal_previous;
+                } else if (typeof refValue === 'undefined' || typeof previous === 'undefined') {
+                    decisionReason = allReasons.no_previous;
+                    this.saved[stickyKey].provider = decisionProvider;
+                } else if (avail[previous].avail < settings.availability_threshold) {
+                    decisionReason = allReasons.previous_below_availability_threshold;
+                    this.saved[stickyKey].provider = decisionProvider;
+                } else if (candidates[decisionProvider].http_rtt < refValue) {
+                    this.saved[stickyKey].provider = decisionProvider;
+                    decisionReason = allReasons.new_provider_below_varianceThreshold;
+                } else {
+                    decisionProvider = previous;
+                    decisionReason = allReasons.choosing_previous_best_perform_within_varianceThreshold;
+                }
+            }
+            else {
+                decisionProvider = getHighest(avail);
+                this.saved[stickyKey].provider = decisionProvider;
+                decisionReason = allReasons.all_providers_eliminated;
+            }
+        }
+
+        response.respond(decisionProvider, cnameOverride || settings.providers[decisionProvider].cname);
+        response.setTTL(settings.default_ttl);
+        response.setReasonCode(decisionReason);
+    };
+
+    /**
+     * @param {!Object} object
+     * @param {Function} filter
+     */
+    function filterObject(object, filter) {
+        var keys = Object.keys(object),
+            i = keys.length,
+            key,
+            data = [];
+        while (i --) {
+            key = keys[i];
+            if (filter(object[key], key)) {
+                data[key] = (object[key]);
+            }
+        }
+        return data;
+    }
+
+    /**
+     * @param {!Object} target
+     * @param {Object} source
+     * @param {string} property
+     */
+    function joinObjects(target, source, property) {
+        var keys = Object.keys(target),
+            i = keys.length,
+            key;
+        while (i --) {
+            key = keys[i];
+            if (typeof source[key] !== 'undefined' && typeof source[key][property] !== 'undefined') {
+                target[key][property] = source[key][property];
+            }
+            else {
+                delete target[key];
+            }
+        }
+        return target;
+    }
+
+    /**
+     * @param {!Object} source
+     * @param {string} property
+     */
+    function getLowest(source, property) {
+        var keys = Object.keys(source),
+            i = keys.length,
+            key,
+            candidate,
+            min = Infinity,
+            value;
+        while (i --) {
+            key = keys[i];
+            value = source[key][property];
+            if (value < min) {
+                candidate = key;
+                min = value;
+            }
+        }
+        return candidate;
+    }
+
+    /**
+     * @param {!Object} source
+     */
+    function getLRUIndex(source) {
+        var keys = Object.keys(source),
+            i = keys.length,
+            key,
+            candidate,
+            min = Infinity,
+            value;
+        while (i --) {
+            key = keys[i];
+            value = source[key].timestamp;
+            if (value < min) {
+                candidate = key;
+                min = value;
+            }
+        }
+        return candidate;
+    }
+
+    function getHighest(source) {
+        var keys = Object.keys(source),
+            i = keys.length,
+            key,
+            candidate,
+            max = -Infinity,
+            value;
+
+        while (i --) {
+            key = keys[i];
+            value = source[key].avail;
+
+            if (value > max) {
+                candidate = key;
+                max = value;
+            }
+        }
+
+        return candidate;
+    }
+
+    function add_rtt_padding(data) {
+        var keys = Object.keys(data),
+            i = keys.length,
+            key;
+
+        while (i --) {
+            key = keys[i];
+            data[key].http_rtt += settings.providers[key].padding;
+        }
+        return data;
+    }
+
+}
---
+++ apps-javascript/sticky-perf-with-penalty-and-availability/test/tests.js
@@ -0,0 +1,406 @@
+
+(function() {
+    'use strict';
+
+    var default_settings = {
+        providers: {
+            'foo': {
+                cname: 'www.foo.com',
+                padding:0
+            },
+            'bar': {
+                cname: 'www.bar.com',
+                padding:10
+            },
+            'baz': {
+                cname: 'www.baz.com',
+                padding:0
+            }
+        },
+        availability_threshold: 60,
+        default_provider: 'foo',
+        default_ttl: 30,
+        error_ttl: 20,
+        sticky_countries: [],
+        variance_threshold: 0.65,
+        maxSavedProviders: 800
+    };
+
+    module('do_init');
+
+    function test_do_init(i) {
+        return function() {
+            var sut,
+                config = {
+                    requireProvider: this.stub()
+                },
+                test_stuff = {
+                    config: config
+                };
+            i.setup(test_stuff);
+            sut = new OpenmixApplication(i.settings || default_settings);
+            // Test
+            sut.do_init(config);
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('default', test_do_init({
+        setup: function() {
+            return;
+        },
+        verify: function(i) {
+            equal(i.config.requireProvider.callCount, 3);
+            equal(i.config.requireProvider.args[2][0], 'foo');
+            equal(i.config.requireProvider.args[1][0], 'bar');
+            equal(i.config.requireProvider.args[0][0], 'baz');
+        }
+    }));
+
+    module('handle_request');
+
+    function test_handle_request(i) {
+        return function() {
+            var sut,
+                config = {
+                    requireProvider: this.stub()
+                },
+                request = {
+                    getProbe: this.stub()
+                },
+                response = {
+                    respond: this.stub(),
+                    setTTL: this.stub(),
+                    setReasonCode: this.stub()
+                },
+                test_stuff;
+
+            sut = new OpenmixApplication(i.settings || default_settings);
+            sut.do_init(config);
+
+            test_stuff = {
+                request: request,
+                response: response,
+                sut: sut
+            };
+
+            i.setup(test_stuff);
+
+            // Test
+            sut.handle_request(request, response);
+
+            // Assert
+            i.verify(test_stuff);
+        };
+    }
+
+    test('test 1 -best_performing provider equal previous only foo available', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 100 },
+                    bar: { avail: 50 },
+                    baz: { avail: 55 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 },
+                    bar: { http_rtt: 98 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'foo', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'A', 'Verifying reason code');
+        }
+    }));
+
+    test('test 2 -no_previous', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 100 },
+                    bar: { avail: 100 },
+                    baz: { avail: 100 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 }, //bestRTT
+                    bar: { http_rtt: 98 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'B', 'Verifying reason code');
+        }
+    }));
+
+    test('test 3 -previous_below_availability_threshold', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 50 },
+                    bar: { avail: 100 },
+                    baz: { avail: 100 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 100 },
+                    bar: { http_rtt: 98 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'foo', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'baz', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.baz.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'C', 'Verifying reason code');
+        }
+    }));
+
+    test('test 4 -new_provider_below_varianceThreshold', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 70 },
+                    bar: { avail: 100 },
+                    baz: { avail: 80 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 },
+                    bar: { http_rtt: 10 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'foo', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'bar', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.bar.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'D', 'Verifying reason code');
+        }
+    }));
+
+    test('test 5 -choosing_previous', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 70 },
+                    bar: { avail: 100 },
+                    baz: { avail: 80 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 },
+                    bar: { http_rtt: 60 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'foo', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'foo', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.foo.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'E', 'Verifying reason code');
+        }
+    }));
+
+    test('test 6 -all_providers_eliminated', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 40 },
+                    bar: { avail: 50 },
+                    baz: { avail: 35 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 },
+                    bar: { http_rtt: 60 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'foo', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.respond.args[0][0], 'bar', 'Verifying selected alias');
+            equal(i.response.respond.args[0][1], 'www.bar.com', 'Verifying CNAME');
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'F', 'Verifying reason code');
+        }
+    }));
+
+    test('test 7 -unexpected_previous_alias', test_handle_request({
+        setup: function(i) {
+            console.log(i);
+            i.request
+                .getProbe
+                .onCall(0)
+                .returns({
+                    foo: { avail: 40 },
+                    bar: { avail: 50 },
+                    baz: { avail: 35 }
+                });
+            i.request
+                .getProbe
+                .onCall(1)
+                .returns({
+                    foo: { http_rtt: 90 },
+                    bar: { http_rtt: 60 },
+                    baz: { http_rtt: 96 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            i.sut.saved['EG-NA-123'] = { 'provider': 'fakeProvider', 'timestamp': 1 };
+        },
+        verify: function(i) {
+            console.log(i);
+            equal(i.response.respond.callCount, 1, 'Verifying respond call count');
+            equal(i.response.setTTL.callCount, 1, 'Verifying setTTL call count');
+            equal(i.response.setReasonCode.callCount, 1, 'Verifying setReasonCode call count');
+
+            equal(i.response.setTTL.args[0][0], 30, 'Verifying TTL');
+            equal(i.response.setReasonCode.args[0][0], 'G', 'Verifying reason code');
+        }
+    }));
+
+    test('clean out this.saved', test_handle_request({
+        settings: {
+            providers: {
+                'foo': {
+                    cname: 'www.foo.com',
+                    padding:0
+                },
+                'bar': {
+                    cname: 'www.bar.com',
+                    padding:0
+                }
+            },
+            availability_threshold: 60,
+            default_provider: 'foo',
+            default_ttl: 30,
+            error_ttl: 20,
+            sticky_countries: [],
+            varianceThreshold: 0.65,
+            maxSavedProviders: 10
+        },
+        setup: function(i) {
+            i.request
+                .getProbe
+                .withArgs('avail')
+                .returns({
+                    foo: { avail: 100 },
+                    bar: { avail: 100 }
+                });
+            i.request
+                .getProbe
+                .withArgs('http_rtt')
+                .returns({
+                    foo: { http_rtt: 140 },
+                    bar: { http_rtt: 98 }
+                });
+            i.request.market = 'EG';
+            i.request.country = 'NA';
+            i.request.asn = 123;
+            var j = 12;
+            while (j--) {
+                i.sut.saved['EG-N' + j + '-123'] = { 'provider': 'foo', 'timestamp': j };
+            }
+            sinon.clock.tick(123);
+        },
+        verify: function(i) {
+            console.log(i.sut.saved);
+            equal(Object.keys(i.sut.saved).length, 10, 'Verifying count of saved object keys');
+            deepEqual(i.sut.saved['EG-N3-123'],  { 'provider': 'foo', 'timestamp': 3 }, 'Verifying saved object #1');
+            deepEqual(i.sut.saved['EG-N11-123'], { 'provider': 'foo', 'timestamp': 11 }, 'Verifying saved object #2');
+            deepEqual(i.sut.saved['EG-NA-123'],  { 'provider': 'bar', 'timestamp': 123 }, 'Verifying saved object #3');
+        }
+    }));
+}());
