e7986e4cd89274e0278beb3e439fd81edf6ea9f4
clarolab1 <dammian_m@hotmail.com>
2016-10-25 10:12:20 -0300

    mixer-with-geo app template update

--- apps-javascript/mixer-with-geo/app.js
+++ apps-javascript/mixer-with-geo/app.js
@@ -12,7 +12,7 @@ var handler = new OpenmixApplication({
             cname: 'cn.baz.net'
         }
     },
-    geo_order: ['state', 'region', 'country', 'market'],
+    geo_order: ['asn', 'state', 'region', 'country', 'market'],
     use_radar_availability_threshold: true,
     use_sonar_availability_threshold: true,
     // default_settings is used to have providers and configuration options for all geos that aren't defined in geo_settings map.
@@ -39,7 +39,7 @@ var handler = new OpenmixApplication({
     },
     // Geo_settings is used to have different providers and configuration options according to the geo and geo type identified.
     // Any values defined here objects will override the higher level settings.
-    // Hierarchy is Global > Market > Country > Region > State.
+    // Hierarchy is Global > Market > Country > Region > State > Asn.
     // All settings are overridable, including providers, paddings, cnames, ttl, threshold, mix, etc.
     //you can also define a 'fallbackBehavior' settings per each geo config to use in case geo config is not avail
     // if you want to define e.g. a new country settings just put the config inside geo_settings -> country
@@ -135,11 +135,44 @@ var handler = new OpenmixApplication({
                 }*/
             }
         },
-        market: {}
-    },
-    // A mapping of ASN codes to ONE provider alias:  asn_overrides: { 123: 'baz', 124: 'bar' },
-    // The providers here should exists in the settings.providers
-    asn_overrides: {}
+        market: {},
+        asn: {
+            '7922': { //Example of Comcast ASN Settings.
+                providers: {
+                    'foo': {
+                        cname: 'cn.foo.net',
+                        kbps_padding: 5,
+                        rtt_padding: 10
+                    },
+                    'baz': {
+                        cname: 'cn.baz.net',
+                        kbps_padding: 5,
+                        rtt_padding: 10
+                    }
+                },
+                default_ttl: 240,
+                radar_availability_threshold: 90,
+                rtt_tp_mix: 0.60,
+                fallbackBehavior: {
+                    providers: {
+                        'baz': {
+                            cname: 'cn.baz.net',
+                            kbps_padding: 0,
+                            rtt_padding: 0
+                        },
+                        'bop': {
+                            cname: 'cn.bop.net',
+                            kbps_padding: 0,
+                            rtt_padding: 0
+                        }
+                    },
+                    default_ttl: 20,
+                    radar_availability_threshold: 85,
+                    rtt_tp_mix: 0.25
+                }
+            }
+        }
+    }
 });
 
 function init(config) {
@@ -192,7 +225,6 @@ function OpenmixApplication(settings) {
             sonarAvailabilityThreshold,
             minRtt,
             rttTpMix,
-            asn = request.asn,
             totalRtt = 0,
             totalKbps = 0,
             selectedCandidates,
@@ -205,7 +237,7 @@ function OpenmixApplication(settings) {
             geo_override_on_region: 'C',
             geo_override_on_country: 'D',
             geo_override_on_market: 'E',
-            asn_override: 'F',
+            geo_override_on_asn: 'F',
             all_providers_eliminated_radar: 'G',
             all_providers_eliminated_sonar: 'H',
             geo_default: 'I',
@@ -362,75 +394,69 @@ function OpenmixApplication(settings) {
             return candidates;
         }
 
-        // ASN override
-        if (settings.asn_overrides[asn] !== undefined) {
-            decisionProvider = settings.asn_overrides[asn];
-            decisionReasons.push(allReasons.asn_override);
-        } else {
-            // Provider eligibility check - filtering per Global_Settings or Geo_Settings
-            candidates = overrideSettingByGeo();
-            selectedCandidates = cloneObject(candidates);
-            candidateAliases = Object.keys(selectedCandidates);
+        // Provider eligibility check - filtering per Global_Settings or Geo_Settings
+        candidates = overrideSettingByGeo();
+        selectedCandidates = cloneObject(candidates);
+        candidateAliases = Object.keys(selectedCandidates);
 
-            if (((settings.use_sonar_availability_threshold === true && Object.keys(dataFusion).length > 0) || settings.use_sonar_availability_threshold === false)
-                && ((settings.use_radar_availability_threshold === true && Object.keys(dataAvail).length > 0) || settings.use_radar_availability_threshold === false)
-                && Object.keys(dataRtt).length > 0) {
+        if (((settings.use_sonar_availability_threshold === true && Object.keys(dataFusion).length > 0) || settings.use_sonar_availability_threshold === false)
+            && ((settings.use_radar_availability_threshold === true && Object.keys(dataAvail).length > 0) || settings.use_radar_availability_threshold === false)
+            && Object.keys(dataRtt).length > 0) {
 
-                candidates = filterAvailRadarSonar(candidates, filterRadarAvailability, filterSonarAvailability); //filter by radar and sonar avail
+            candidates = filterAvailRadarSonar(candidates, filterRadarAvailability, filterSonarAvailability); //filter by radar and sonar avail
+            candidateAliases = Object.keys(candidates);
+
+            if (candidateAliases.length === 0 && fallbackBehavior !== undefined &&
+                fallbackBehavior.providers !== undefined && Object.keys(fallbackBehavior.providers).length > 0 ) {
+                overrideFallbackSettingsByGeo(); //override fallback behavior settings
+                selectedCandidates = cloneObject(candidates);
+                candidates = filterAvailRadarSonar(candidates, filterRadarAvailability, filterSonarAvailability);
                 candidateAliases = Object.keys(candidates);
-
-                if (candidateAliases.length === 0 && fallbackBehavior !== undefined &&
-                    fallbackBehavior.providers !== undefined && Object.keys(fallbackBehavior.providers).length > 0 ) {
-                    overrideFallbackSettingsByGeo(); //override fallback behavior settings
-                    selectedCandidates = cloneObject(candidates);
-                    candidates = filterAvailRadarSonar(candidates, filterRadarAvailability, filterSonarAvailability);
-                    candidateAliases = Object.keys(candidates);
-                }
-
-                if (candidateAliases.length === 0) {
-                    // Join the avail scores with the list of viable candidates
-                    candidates = intersectObjects(cloneObject(selectedCandidates), dataAvail, 'avail');
-                    candidateAliases = Object.keys(candidates);
-                    if (candidateAliases.length > 0) {
-                        decisionProvider = getHighest(candidates, 'avail');
-                    }
-                } else if (candidateAliases.length === 1) {
-                    decisionProvider = candidateAliases[0];
-                    decisionReasons.push(allReasons.only_one_provider_avail);
-                } else {
-                    // Join the rtt scores with the list of viable candidates
-                    candidates = intersectObjects(candidates, dataRtt, 'http_rtt');
-
-                    if (rttTpMix !== 1) {
-                        candidates = filterObject(candidates, filterInvalidRttScores);
-                    }
-
-                    if (Object.keys(dataKbps).length > 0) {
-                        // Join the kbps scores with the list of viable candidates
-                        candidates = intersectObjects(candidates, dataKbps, 'http_kbps');
-                    }
-
-                    candidateAliases = Object.keys(candidates);
-                    if (candidateAliases.length > 0) {
-
-                        // Add kbps and rtt padding
-                        candidates = addPadding(candidates);
-
-                        // mean-normalized RTT and TP and calculate scores
-                        candidates = calculateScore(candidates);
-
-                        decisionProvider = getHighest(candidates, 'score');
-                        decisionReasons.push(allReasons.optimum_server_chosen);
-                    }
-                }
             }
 
-            if (decisionProvider === undefined) {
-                candidates = fallbackBehavior && fallbackBehavior.providers && Object.keys(fallbackBehavior.providers).length > 0 ? fallbackBehavior.providers : settings.default_settings.providers;
+            if (candidateAliases.length === 0) {
+                // Join the avail scores with the list of viable candidates
+                candidates = intersectObjects(cloneObject(selectedCandidates), dataAvail, 'avail');
                 candidateAliases = Object.keys(candidates);
-                decisionProvider = candidateAliases[Math.floor(Math.random() * candidateAliases.length)];
-                decisionReasons.push(allReasons.data_problem);
+                if (candidateAliases.length > 0) {
+                    decisionProvider = getHighest(candidates, 'avail');
+                }
+            } else if (candidateAliases.length === 1) {
+                decisionProvider = candidateAliases[0];
+                decisionReasons.push(allReasons.only_one_provider_avail);
+            } else {
+                // Join the rtt scores with the list of viable candidates
+                candidates = intersectObjects(candidates, dataRtt, 'http_rtt');
+
+                if (rttTpMix !== 1) {
+                    candidates = filterObject(candidates, filterInvalidRttScores);
+                }
+
+                if (Object.keys(dataKbps).length > 0) {
+                    // Join the kbps scores with the list of viable candidates
+                    candidates = intersectObjects(candidates, dataKbps, 'http_kbps');
+                }
+
+                candidateAliases = Object.keys(candidates);
+                if (candidateAliases.length > 0) {
+
+                    // Add kbps and rtt padding
+                    candidates = addPadding(candidates);
+
+                    // mean-normalized RTT and TP and calculate scores
+                    candidates = calculateScore(candidates);
+
+                    decisionProvider = getHighest(candidates, 'score');
+                    decisionReasons.push(allReasons.optimum_server_chosen);
+                }
             }
+        }
+
+        if (decisionProvider === undefined) {
+            candidates = fallbackBehavior && fallbackBehavior.providers && Object.keys(fallbackBehavior.providers).length > 0 ? fallbackBehavior.providers : settings.default_settings.providers;
+            candidateAliases = Object.keys(candidates);
+            decisionProvider = candidateAliases[Math.floor(Math.random() * candidateAliases.length)];
+            decisionReasons.push(allReasons.data_problem);
         }
 
         cname = selectedCandidates
