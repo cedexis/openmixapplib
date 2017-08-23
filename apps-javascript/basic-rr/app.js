4642e3f39053693f018ac1ac100964cb8e889c76
Gaston <gaston@cedexis.com>
2017-08-23 12:45:44 -0300

    basic rr template

---
+++ apps-javascript/basic-rr/app.js
@@ -0,0 +1,68 @@
+var handler = new OpenmixApplication({
+    /**
+     * The list of available CNAMEs, keyed by alias.
+     */
+    providers: {
+        'foo': {
+            cname: 'www.foo.com'
+        },
+        'bar': {
+            cname: 'www.bar.com'
+        },
+        'baz': {
+            cname: 'www.baz.com'
+        }
+    },
+    // The TTL to be set when the application chooses a geo provider.
+    default_ttl: 20
+});
+
+function init(config) {
+    'use strict';
+    handler.do_init(config);
+}
+
+function onRequest(request, response) {
+    'use strict';
+    handler.handle_request(request, response);
+}
+
+/** @constructor */
+function OpenmixApplication(settings) {
+    'use strict';
+
+    var aliases = settings.providers === undefined ? [] : Object.keys(settings.providers);
+
+    /**
+     * @param {OpenmixConfiguration} config
+     */
+    this.do_init = function(config) {
+        var i = aliases.length;
+        // Register the providers that will be selected from
+        while (i --) {
+            config.requireProvider(aliases[i]);
+        }
+    };
+
+    /**
+     * @param {OpenmixRequest} request
+     * @param {OpenmixResponse} response
+     */
+    this.handle_request = function(request, response) {
+        var allReasons,
+            decisionProvider,
+            decisionReason;
+
+        allReasons = {
+            random_selection: 'A'
+        };
+
+        decisionProvider = aliases[Math.floor(Math.random() * aliases.length)];
+        decisionReason = allReasons.random_selection;
+
+        response.respond(decisionProvider, settings.providers[decisionProvider].cname);
+        response.setTTL(settings.default_ttl);
+        response.setReasonCode(decisionReason);
+    };
+
+}
